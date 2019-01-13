d000cef7a9154a421ae58b4559ba3562610c6a2e
Toni-Ty <antonique2479@yahoo.com>
2019-01-13 05:08:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,4 +8,6 @@ def my_collect(collection)
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) {|student| " #{student.split(" ").first}" }
+my_collect(array) do |student| 
+  student.split(" ").first
+end
